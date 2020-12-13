5a537d786b5ec6cf2102c656881a795eed54e3b7
Rohini Mishra <iit2018087@iiita.ac.in>
2020-12-14 03:32:07 +0530

    nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -28,7 +28,7 @@ import {
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <NavLink href="https://github.com/Rohini13">Github</NavLink>
+                                <NavLink href="https://github.com/Rohini13/Shopping-List">Github</NavLink>
                             </NavItem>
                         </Nav>
                     </Collapse>
